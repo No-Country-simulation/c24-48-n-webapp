8fcba6f35e671155eae826198dfd9d071eab78e1
Johann Gaviria <johannmgr@gmail.com>
2025-02-19 18:22:34 -0500

    feat(project): crear estructura inicial del proyecto
    - Crear estructura del backend
    - Crear estructura del frontend

---
+++ backend/config/settings/development.py
@@ -0,0 +1,28 @@
+from config.settings.base import *
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(Path.joinpath(BASE_DIR,'.env'))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0']
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
---
+++ backend/config/urls.py
@@ -0,0 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+
+urlpatterns = [] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
