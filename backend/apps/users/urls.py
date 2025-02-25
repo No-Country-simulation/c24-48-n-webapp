29fe326278e1666206e9337f120403120b643568
paolo <paolo_delpiano@hotmail.com>
2025-02-25 12:19:21 -0300

    feat(users):creacion de logout-Correccion_CRUD

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,9 +3,9 @@ from .views import user_register, user_update, user_login, user_delete, user_log
 
 
 urlpatterns = [
-    path('user/register/', user_register, name = 'register_user_api'),
-    path('user/update/<int:pk>/', user_update, name = 'update_user_api'),
-    path('user/delete/<int:pk>/', user_delete, name = 'delete_user_api'),
-    path('user/login/', user_login, name = 'login_user_api'),
-    path('user/logout/', user_logout, name = 'logout_user_api'),
+    path('user/register/', user_register, name = 'register_user'),
+    path('user/update/<int:pk>/', user_update, name = 'update_user'),
+    path('user/delete/<int:pk>/', user_delete, name = 'delete_user'),
+    path('user/login/', user_login, name = 'login_user'),
+    path('user/logout/', user_logout, name = 'logout_user'),
     ]
