789e7d6adcfde911bf82f509b9c2c779e4b305d5
paolo <paolo_delpiano@hotmail.com>
2025-02-26 10:58:27 -0300

    local-borre pests creado por mi

--- backend/apps/pets/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import pet_register, pet_update, pet_delete
-
-
-urlpatterns = [
-    path('pet/register/', pet_register, name = 'pet_register'),
-    path('pet/update/<int:pk>/', pet_update, name = 'pet_update'),
-    path('pet/delete/<int:pk>/', pet_delete, name = 'pet_delete'),
-    ]
