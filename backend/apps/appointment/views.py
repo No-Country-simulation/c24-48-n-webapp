dd6724c76c07537a6d9264904bc2a013c0533ef2
Johann Gaviria <johannmgr@gmail.com>
2025-02-26 19:28:34 -0500

    feat(appointment): crear endpoint para aceptar o rechazar citas

--- backend/apps/appointment/views.py
+++ backend/apps/appointment/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from apps.core.utils.serializer_validation import validate_serializer
 from .serializers import AppointmentValidationSerializer
+from .models import Appointment
 
 
 # Endpoint para agendar citas
@@ -31,3 +32,41 @@ def schedule_appointment(request):
         'status': 'success',
         'message': 'Appointment successfully scheduled.'
     }, status=status.HTTP_201_CREATED)
+
+
+# Endpoint para aceptar o rechazar citas
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def accept_reject_appointments(request, appointment_id):
+    try:
+        # Obtiene la cita por su ID
+        appointment = Appointment.objects.get(id=appointment_id)
+    except Appointment.DoesNotExist:
+        # Respuesta de error al no encontrar la cita
+        return Response({
+            'status': 'error',
+            'message': 'Appointment not found.'
+        }, status=status.HTTP_404_NOT_FOUND)
+
+    # Obtiene los datos enviados en la peticion
+    status_action = request.data.get('status')
+
+    # Verifica si el estado es el correcto
+    if status_action not in ['CONFIRMED', 'CANCELLED']:
+
+        # Respuesta de error al no tener el formato correcto
+        return Response({
+            'status': 'error',
+            'message': 'Invalid status. Use "CONFIRMED" or "CANCELLED".'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    # Guarda el estado de la cita en la base de datos
+    appointment.status = status_action
+    appointment.save()
+
+    # Respuesta exitosa al cambiar el estado de la cita
+    return Response({
+        'status': 'success',
+        'message': f'Appointment successfully {status_action.lower()}.'
+    }, status=status.HTTP_200_OK)
